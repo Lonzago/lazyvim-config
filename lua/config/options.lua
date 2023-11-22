3ba6cd8e75f73db788e11974a67cbc981997a905
GonDev <gonzalodvrdev@gmail.com>
2023-11-22 17:16:16 +0000

    Remove relative numbers & some windows options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Windows
+vim.opt.number = true
+vim.opt.colorcolumn = "+1"
+vim.opt.list = true
+vim.opt.signcolumn = 'yes:1'
+vim.opt.relativenumber = false
+vim.opt.cursorline = true
