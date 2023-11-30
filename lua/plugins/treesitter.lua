9769f1db801bb8958e265f227fe19ec3cbec9c66
GonDev <gonzalodvrdev@gmail.com>
2023-11-30 00:27:14 +0000

    Added new plugin Treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,40 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	event = "VeryLazy",
+	main = "nvim-treesitter.configs",
+	opts = {
+		ensure_installed = {
+			"lua",
+			"luadoc",
+			
+			"dockerfile",
+			"bash",
+			
+			"cpp",
+			"python",
+
+			"html",
+			"css",
+			"http",
+			"javascript",
+			"json",
+
+			"typescript",
+			"typoscript"
+
+			"markdown_inline",
+			"vim",
+			"vimdoc",
+			"vue",
+			"yaml",
+			"xml",
+		},
+		highlight = {
+			enable = true,
+		},
+		ident = {
+			enable = true,
+		},
+	},
+}
