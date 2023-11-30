d52f62ed5cc7fe2259e60089f32d2ce10c7f22f8
GonDev <gonzalodvrdev@gmail.com>
2023-11-30 02:19:47 +0000

    Added keymaps for: close-buffer, save-file and exit

--- lua/config/keymapping.lua
+++ lua/config/keymapping.lua
@@ -1,2 +1,10 @@
 
-vim.keymap.set("n", "<C-s>",":w<CR>")
+-- Get out Q
+vim.keymap.set("n", "Q", ":q<CR>")
+-- Save File
+vim.keymap.set({"n","i","v"}, "<C-s>",":w<CR>", { desc = "Save file" })
+
+
+----------------- BUFFERS ----------------
+-- close buffer
+vim.keymap.set("n", "<leader>q", "<cmd>bd<CR>", { desc = "Close Buffer" })
