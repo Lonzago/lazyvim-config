46224e257b72674edd9648195a0bfcac1fcf1e28
GonDev <gonzalodvrdev@gmail.com>
2023-11-30 01:59:07 +0000

    Added new plugin Telescope

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,42 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  event = "VeryLazy",
+  dependencies = { 
+    'nvim-lua/plenary.nvim',
+  },
+  opts = {
+    extensions = {
+      fzf = {
+        fuzzy = true,
+        override_generic_sorter = true,
+        override_file_sorter = true,
+        case_mode = "smart_case",
+      },
+    },
+  },
+  config = function(opts)
+    require('telescope').setup(opts)
+    require('telescope').load_extension('fzf')
+  end,
+  keys = {
+    {
+      "<leader>pp", function()
+        require('telescope.builtin').find_files()
+      end,
+      desc = "Telescope Find Files"
+    },
+    {
+      "<leader>pg", function() 
+        require('telescope.builtin').git_files({show_untracked = true})
+      end,
+      desc = "Telescope Git Files",
+    },
+    {
+      "<leader>pe", function() 
+        require('telescope.builtin').buffers()
+      end,
+      desc = "Telescope buffers"
+      
+    },
+  },
+}
