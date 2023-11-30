86b022c0cc68452ce590dee6576e9cbad3d92517
GonDev <gonzalodvrdev@gmail.com>
2023-11-30 03:22:45 +0000

    Added more keymaps

--- lua/config/keymapping.lua
+++ lua/config/keymapping.lua
@@ -4,6 +4,22 @@ vim.keymap.set("n", "Q", ":q<CR>")
 -- Save File
 vim.keymap.set({"n","i","v"}, "<C-s>",":w<CR>", { desc = "Save file" })
 
+-- Undo
+vim.keymap.set({"n","i","v"}, "<C-z>",":u<CR>", { desc = "Undo action" })
+
+
+-- Select all
+vim.keymap.set("n", "<C-a>", "gg<S-v>G")
+
+
+-- Stay in indent mode
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
+-- Move block
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move Block Down" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move Block Up" })
+
+
 
 ----------------- BUFFERS ----------------
 -- close buffer
